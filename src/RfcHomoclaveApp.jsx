77fdc8a30dca8396bd6d0461a5552481204e6115
king-beard <chucho_herrera10@icloud.com>
2025-04-09 18:32:28 -0600

    update. - add cors -add required fields in record

--- src/RfcHomoclaveApp.jsx
+++ src/RfcHomoclaveApp.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import { ConsultRfcHomoclave } from "./components/ConsultRfcHomoclave"
+import { ShowRfcHomoclave } from "./components/ShowRfcHomoclave";
+
+const initalRequest = {
+  method: "POST",
+  mode: 'cors',
+  headers: {
+    "Content-Type": "application/json",
+  }
+};
 
 export const RfcHomoclaveApp = () => {
+
+  const [request, setRequest] = useState(initalRequest)
+
+  const onConsultRfcHomoclave = (value) => {
+    setRequest(value)
+  }
+
   return (
     <div className="container mt-5">
-      <ConsultRfcHomoclave />
+      <div className="row d-flex justify-content-center">
+        <div className="col-12 col-sm-10 col-md-8">
+          <h3 >Calcula RFC con homoclave</h3>
+          <ConsultRfcHomoclave onConsultRfcHomoclave={value => onConsultRfcHomoclave(value) } />
+          <ShowRfcHomoclave bodyRequest={request} />
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/ConsultRfcHomoclave.jsx
+++ src/components/ConsultRfcHomoclave.jsx
@@ -6,67 +6,75 @@ const initialForm = {
   name: "",
   fathersLastName: "",
   mothersLastName: "",
-  birthdayDate: "12-03-1997",
+  birthdayDate: "",
   personType: "fisica"
 }
 
-export const ConsultRfcHomoclave = () => {
+export const ConsultRfcHomoclave = ({onConsultRfcHomoclave}) => {
   const { formState, onInputChange, onResetForm } = useForm(initialForm);
   const { name, fathersLastName, mothersLastName, birthdayDate } = formState;
-  const [isRender, setIsRender] = useState(false)
+  // const [isRender, setIsRender] = useState(false)
 
-  const onSubmit = () => {
+  const onSubmit = (event) => {
     event.preventDefault()
-    setIsRender(true)
+    onConsultRfcHomoclave(formState)
+    // setIsRender(true)
   }
 
-  const onCleanForm = () => {
+  const onCleanForm = (event) => {
     event.preventDefault()
-    setIsRender(false)
     onResetForm(initialForm)
   }
 
   return (
-    <form>
-      <input
-        type="text"
-        className="form-control mb-2"
-        placeholder="Nombre"
-        name="name"
-        value={name}
-        onChange={onInputChange}
-      />
-      <input
-        type="text"
-        className="form-control mb-2"
-        placeholder="Apellido Paterno"
-        name="fathersLastName"
-        value={fathersLastName}
-        onChange={onInputChange}
-      />
-      <input
-        type="text"
-        className="form-control mb-2"
-        placeholder="Apellido Materno"
-        name="mothersLastName"
-        value={mothersLastName}
-        onChange={onInputChange}
-      />
-      <input
-        type="text"
-        className="form-control mb-2"
-        placeholder="Fecha de nacimiento"
-        name="date"
-        value={birthdayDate}
-        onChange={onInputChange}
-      />
-      <div className="input-group date">
-        <input type="text" className="form-control" />
-        <div className="input-group-text"><i className="fa fa-calendar"></i></div>
+    <form className="mt-4">
+      <div className="mb-2">
+        <label className="form-label">Nombre(s)</label>
+        <input
+          type="text"
+          className="form-control"
+          name="name"
+          value={name}
+          onChange={onInputChange}
+        />
       </div>
-      <button type="submit" className="btn btn-outline-danger" onClick={onSubmit}>Calcular</button>
-      <button type="submit" className="btn btn-outline-danger" onClick={onCleanForm}>Limpiar Formulario</button>
-      {isRender && <ShowRfcHomoclave bodyRequest={formState} />}
+      <div className="mb-2">
+        <label className="form-label">Apellido Paterno</label>
+        <input
+          type="text"
+          className="form-control"
+          name="fathersLastName"
+          value={fathersLastName}
+          onChange={onInputChange}
+        />
+      </div>
+      <div className="mb-2">
+        <label className="form-label">Apellido Materno</label>
+        <input
+          type="text"
+          className="form-control"
+          name="mothersLastName"
+          value={mothersLastName}
+          onChange={onInputChange}
+        />
+      </div>
+      <div className="mb-2">
+        <label className="form-label">Fecha de nacimiento (DD-MM-YYYY)</label>
+        <input
+          type="text"
+          className="form-control"
+          name="birthdayDate"
+          value={birthdayDate}
+          onChange={onInputChange}
+        />
+      </div>
+
+      <div className="d-flex justify-content-center mt-5">
+        <button type="submit" className="btn btn-outline-danger" onClick={onSubmit}>Calcular</button>
+        <button type="submit" className="btn btn-outline-danger" onClick={onCleanForm}>Limpiar Formulario</button>
+      </div>
+
+      {/* {isRender && <ShowRfcHomoclave bodyRequest={formState} />} */}
     </form>
   )
 }
